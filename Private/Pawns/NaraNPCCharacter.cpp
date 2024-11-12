cdcb14709915cd57e85a16ad6aed7ababa29273a
Fabian F <fabian.froding@gmail.com>
2024-11-12 22:03:08 +0100

    Added music manager, added AI states
     - Added music manager base class
     - Added basic AI states and some other AI related variables to NPC base class used in BPs

--- Private/Pawns/NaraNPCCharacter.cpp
+++ Private/Pawns/NaraNPCCharacter.cpp
@@ -1,3 +1,8 @@
 // Copyright Fabian Fröding 2024. All Right Reserved.
 
 #include "Pawns/NaraNPCCharacter.h"
+
+void ANaraNPCCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
--- Public/Pawns/NaraNPCCharacter.h
+++ Public/Pawns/NaraNPCCharacter.h
@@ -7,9 +7,31 @@
 
 #include "NaraNPCCharacter.generated.h"
 
+UENUM(BlueprintType)
+enum class E_NaraAIState : uint8
+{
+	E_Default	UMETA(DisplayName = "Default"),
+	E_Idle		UMETA(DisplayName = "Idle"),
+	E_Chase		UMETA(DisplayName = "Chase")
+};
+
 UCLASS()
 class NARA_API ANaraNPCCharacter : public ANaraCharacter
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(BlueprintReadWrite)
+	E_NaraAIState AIState = E_NaraAIState::E_Idle;
+
+	UPROPERTY(BlueprintReadWrite)
+	int32 CurrentPatrolIndex = 0;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FVector> PatrolLocations;
+
+protected:
+	virtual void BeginPlay() override;
 	
 };
