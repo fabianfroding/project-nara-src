f151f0536a110661f5dccb14afe43ed9fbc5d941
Fabian F <fabian.froding@gmail.com>
2024-12-12 22:16:35 +0100

    NetSerialize and StructOpsTypeTraits
     - Added NetSerialize and StructOpsTypeTraits for custom GameplayEffectContext type

--- Public/Types/NaraAbilityTypes.h
+++ Public/Types/NaraAbilityTypes.h
@@ -28,10 +28,33 @@ public:
 	/* Returns the actual struct used for serialization, subclasses must override this! */
 	virtual UScriptStruct* GetScriptStruct() const
 	{
-		return FGameplayEffectContext::StaticStruct();
+		return StaticStruct();
+	}
+
+	/** Creates a copy of this context, used to duplicate for later modifications */
+	virtual FNaraGameplayEffectContext* Duplicate() const
+	{
+		FNaraGameplayEffectContext* NewContext = new FNaraGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
 	}
 
 	/* Custom serialization, subclasses must override this */
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
 
+};
+
+template<>
+struct TStructOpsTypeTraits<FNaraGameplayEffectContext> : TStructOpsTypeTraitsBase2<FNaraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true
+	};
 };
