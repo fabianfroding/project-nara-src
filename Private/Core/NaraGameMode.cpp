1556252ab2b5679acfa10cd59c94a59524be4a48
Fabian F <fabian.froding@gmail.com>
2025-01-19 21:46:11 +0100

    Started setting up save game logic

--- Private/Core/NaraGameMode.cpp
+++ Private/Core/NaraGameMode.cpp
@@ -1,3 +1,16 @@
 // Copyright Fabian Fröding 2024. All Right Reserved.
 
 #include "Core/NaraGameMode.h"
+
+#include "Core/NaraSaveGame.h"
+#include "Kismet/GameplayStatics.h"
+
+void ANaraGameMode::SaveSlotData(const FString SlotName, const int32 SlotIndex)
+{
+	if (UGameplayStatics::DoesSaveGameExist(SlotName, SlotIndex))
+		UGameplayStatics::DeleteGameInSlot(SlotName, SlotIndex);
+	
+	USaveGame* SaveGameObject = UGameplayStatics::CreateSaveGameObject(SaveGameClass);
+	UNaraSaveGame* NaraSaveGame = Cast<UNaraSaveGame>(SaveGameObject);
+	UGameplayStatics::SaveGameToSlot(NaraSaveGame, SlotName, SlotIndex);
+}
--- Public/Core/NaraGameMode.h
+++ Public/Core/NaraGameMode.h
@@ -7,9 +7,19 @@
 
 #include "NaraGameMode.generated.h"
 
+class USaveGame;
+
 UCLASS(minimalapi)
 class ANaraGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<USaveGame> SaveGameClass;
+
+public:
+	// Call this from the main menu when a new game is created.
+	void SaveSlotData(const FString SlotName, const int32 SlotIndex);
 
 };
