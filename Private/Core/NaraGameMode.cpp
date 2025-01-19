0333f4c2f0258d57c522f9ec0377b6b52cdbbd40
Fabian F <fabian.froding@gmail.com>
2025-01-19 22:44:23 +0100

    Finished save game backend logic

--- Private/Core/NaraGameMode.cpp
+++ Private/Core/NaraGameMode.cpp
@@ -5,12 +5,46 @@
 #include "Core/NaraSaveGame.h"
 #include "Kismet/GameplayStatics.h"
 
+void ANaraGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	Maps.Add(DefaultMapName, DefaultMap);
+}
+
+void ANaraGameMode::DeleteSaveSlot(const FString& SlotName, int32 SlotIndex)
+{
+	if (UGameplayStatics::DoesSaveGameExist(SlotName, SlotIndex))
+		UGameplayStatics::DeleteGameInSlot(SlotName, SlotIndex);
+}
+
 void ANaraGameMode::SaveSlotData(const FString SlotName, const int32 SlotIndex)
 {
 	if (UGameplayStatics::DoesSaveGameExist(SlotName, SlotIndex))
 		UGameplayStatics::DeleteGameInSlot(SlotName, SlotIndex);
 	
 	USaveGame* SaveGameObject = UGameplayStatics::CreateSaveGameObject(SaveGameClass);
+	
 	UNaraSaveGame* NaraSaveGame = Cast<UNaraSaveGame>(SaveGameObject);
+	NaraSaveGame->bSlotOccupied = true;
+	//NaraSaveGame->MapName = LoadSlot->GetMapName();
+
 	UGameplayStatics::SaveGameToSlot(NaraSaveGame, SlotName, SlotIndex);
+}
+
+UNaraSaveGame* ANaraGameMode::GetSaveSlotData(const FString& SlotName, int32 SlotIndex) const
+{
+	USaveGame* SaveGameObject = nullptr;
+
+	if (UGameplayStatics::DoesSaveGameExist(SlotName, SlotIndex))
+		SaveGameObject = UGameplayStatics::LoadGameFromSlot(SlotName, SlotIndex);
+	else
+		SaveGameObject = UGameplayStatics::CreateSaveGameObject(SaveGameClass);
+
+	UNaraSaveGame* NaraSaveGame = Cast<UNaraSaveGame>(SaveGameObject);
+	return NaraSaveGame;
+}
+
+void ANaraGameMode::TravelToMap(const FString MapName)
+{
+	UGameplayStatics::OpenLevelBySoftObjectPtr(this, Maps.FindChecked(MapName));
 }
--- Public/Core/NaraGameMode.h
+++ Public/Core/NaraGameMode.h
@@ -7,6 +7,7 @@
 
 #include "NaraGameMode.generated.h"
 
+class UNaraSaveGame;
 class USaveGame;
 
 UCLASS(minimalapi)
@@ -18,8 +19,26 @@ protected:
 	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<USaveGame> SaveGameClass;
 
+	UPROPERTY(EditDefaultsOnly)
+	FString DefaultMapName;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UWorld> DefaultMap;
+
+	UPROPERTY(EditDefaultsOnly)
+	TMap<FString, TSoftObjectPtr<UWorld>> Maps;
+
+protected:
+	virtual void BeginPlay() override;
+
 public:
+	static void DeleteSaveSlot(const FString& SlotName, int32 SlotIndex);
+
 	// Call this from the main menu when a new game is created.
 	void SaveSlotData(const FString SlotName, const int32 SlotIndex);
+
+	UNaraSaveGame* GetSaveSlotData(const FString& SlotName, int32 SlotIndex) const;
+
+	void TravelToMap(const FString MapName);
 
 };
